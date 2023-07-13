2713559cd220a02ad7890611aa1e308a7eab2618
Charlie <itscharlieliu@gmail.com>
2023-07-12 21:11:48 -0700

    upload image to s3 and use it in the site

--- src/components/pages/MainPage.tsx
+++ src/components/pages/MainPage.tsx
@@ -5,6 +5,13 @@ import "./MainPage.css";
 export const MainPage = () => {
   return (
     <div className="main-page">
+      <img
+        alt="background"
+        className="main-page__decoration"
+        src="https://charlieliu-home-page.s3.us-west-1.amazonaws.com/DSC_0455.jpg"
+        //   width={200}
+        //   height={200}
+      />
       <div className="main-page__about-text">
         <Typography variant="h4">
           Transforming visions into captivating digital experiences through code
