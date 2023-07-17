47e202d4a1f3d5a4348ce30ee1b05ba9bf3611fa
Charlie <itscharlieliu@gmail.com>
2023-07-16 20:46:13 -0700

    set up routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import { Header } from "./components/common/Header";
 import { MainPage } from "./components/pages/MainPage";
 import "./App.css";
+import { RouterProvider } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainPage />,
+  },
+  {
+    path: "/test",
+    element: <div>test</div>,
+  },
+]);
 
 function App() {
   // TODO add routing
   return (
     <div className="app">
       <Header />
-      <MainPage />
+      <RouterProvider router={router} />
     </div>
   );
 }
